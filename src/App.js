0fdebf94e02aedd3ff79db3062ad258f974214da
Ivan Starčević <ivan.starcevic15@gmail.com>
2022-05-17 21:28:53 +0200

    Create rating select element, about page and use React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  NavLink,
+} from "react-router-dom";
 
+import Card from "./components/shared/Card";
 import Header from "./components/Header/Header";
 import FeedbackStats from "./components/FeedbackStats/FeedbackStats";
 import FeedbackList from "./components/FeedbackList/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
+import FeedbackForm from "./components/FeedbackForm/FeedbackForm";
+import About from "./pages/About";
+import AboutIconLink from "./components/AboutIconLink/AboutIconLink";
 
 const App = () => {
   const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback]);
+  };
+
   const deleteFeedback = (id) => {
     if (window.confirm("Are you sure you want to delete?")) {
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Card>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+          <NavLink to="/about" activeClassName="active">
+            About
+          </NavLink>
+        </Card>
+
+        <AboutIconLink />
       </div>
-    </>
+    </Router>
   );
 };
 
